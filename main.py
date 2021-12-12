704540c1ae83fa54b26ee0aa73bc67e5bb044478
lovelyoyrmia <mokalulovelyo@gmail.com>
2021-12-12 20:51:32 +0700

    create streamlit app

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 import streamlit as st
+from PIL import Image
+
+
+@st.cache()
+def load_image(img):
+    image = Image.open(img)
+    return image
+
+st.title('Fashion recommender system')
+
+file_upload = st.file_uploader('Browse your photo', type=['jpg', 'png', 'jpeg'])
+
+if file_upload is not None:
+    image = load_image(file_upload)
+    st.image(image, use_column_width=True)
 
 
